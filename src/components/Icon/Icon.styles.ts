92e1a6c52a8d955ba148727ae88f29133a1091ef
Agonys <toxic325@gmail.com>
2021-12-01 02:05:35 +0100

    [SWA-7] fix: PR fix

--- src/components/Icon/Icon.styles.ts
+++ src/components/Icon/Icon.styles.ts
@@ -6,7 +6,4 @@ export const Box = styled.div<IPropsBox>`
   color: white;
   transition: transform ${({ theme }) => theme.transitions.ease};
   transform: rotate(${({ rotate }) => (rotate ? "180deg" : "0deg")});
-
-  svg {
-  }
 `;
